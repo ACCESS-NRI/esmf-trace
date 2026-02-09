63375025e03a236ba29abe38a7f29f0677f3e231
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2026-02-09 11:12:26 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 set -euo pipefail
 
 # Gadi setup for esmf-trace
-module load python3/3.11.7
 module use /g/data/vk83/modules
 module load model-tools/babeltrace2/2.1.2
 
 # Create venv and install
 python3 -m venv .venv
 . .venv/bin/activate
+
+pip install --upgrade pip setuptools wheel
+
 pip install -r requirements.txt
+
+pip install -r requirements-access.txt
+
 pip install -e .
