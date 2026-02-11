17bde313143009c20aad4c40958bd8364950a9d9
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2026-02-11 14:24:39 +1100

    Update associated setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ pip install --upgrade pip setuptools wheel
 
 pip install -r requirements.txt
 
+# These are not merged yet, but we want to be able to install them in the meantime,
+# because they are needed for the ARE work.
 pip install -r requirements-access.txt
 
+# Install and try to pull the "access" extras if available
 pip install -e .
+pip install -e ".[access]" || true
