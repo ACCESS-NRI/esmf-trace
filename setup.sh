fd47a75893ec9c6897c3d224db7cabab11334a16
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2026-02-12 14:58:33 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,11 @@ python3 -m venv .venv
 
 pip install --upgrade pip setuptools wheel
 
-pip install -r requirements.txt
-
-# These are not merged yet, but we want to be able to install them in the meantime,
-# because they are needed for the ARE work.
-pip install -r requirements-access.txt
-
-# Install and try to pull the "access" extras if available
+# Install the package in editable mode for development
 pip install -e .
-pip install -e ".[access]" || true
+
+# Install the interactive tooling for the notebooks
+pip install -e ".[interactive]"
+
+# Workspace bundle: install other ACCESS repos into the same venv for workflow convenience
+pip install -r requirements-access.txt
